e50fe776fe548e0089927d69f7f129e6016dfed9
Mike Lang <mlang@student.neumont.edu>
2016-06-30 21:36:53 -0600

    Built up TimeCard more - Added CalculateOvertime and other utility methods

--- Time Sheet/Time Sheet/TimeCard.cs
+++ Time Sheet/Time Sheet/TimeCard.cs
@@ -38,8 +38,60 @@ namespace Time_Sheet
                 if (SameDay(date, _days[x].GetDate()))
                 {
                     _days[x].SetHours(type, hours);
+                    break;
                 }
             }
+            throw new ArgumentOutOfRangeException("date", "The provided date was not found within the TimeCard");
+        }
+        public float GetHours(DateTime date, Day.HourType type, float hours)
+        {
+            for (int x = 0; x < 14; x++)
+            {
+                if (SameDay(date, _days[x].GetDate()))
+                {
+                    return _days[x].GetHours(type);
+                }
+            }
+            throw new ArgumentOutOfRangeException("date", "The provided date was not found within the TimeCard");
+        }
+
+        public float CalculateOvertime()
+        {
+            float RegularHours = 0;
+            float OvertimeHours = 0;
+
+            for (int x = 0; x < 7; x++)
+            {
+                RegularHours += _days[x].GetHours(Day.HourType.WORKING);
+            }
+            if(RegularHours > 40)
+            {
+                OvertimeHours += RegularHours - 40;
+            }
+
+            RegularHours = 0;
+            for (int x = 7; x < 14; x++)
+            {
+                RegularHours += _days[x].GetHours(Day.HourType.WORKING);
+            }
+            if (RegularHours > 40)
+            {
+                OvertimeHours += RegularHours - 40;
+            }
+
+            return OvertimeHours;
+        }
+
+        public float GetTotalHours(Day.HourType type)
+        {
+            float RunningTotal = 0;
+
+            for (int x = 0; x < 14; x++)
+            {
+                RunningTotal += _days[x].GetHours(type);
+            }
+
+            return RunningTotal;
         }
     }
 }
