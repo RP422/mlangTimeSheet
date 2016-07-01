45d958433ef40bd6edf35a2ef815e6feb5720fe8
Mike Lang <mlang@student.neumont.edu>
2016-06-30 22:26:40 -0600

    Glory upon us! TimeCardTests has been made and outlined!

---
+++ Time Sheet/Time SheetTests/TimeCardTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Time_Sheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Sheet.Tests
+{
+    [TestClass()]
+    public class TimeCardTests
+    {
+        [TestMethod()]
+        public void ValidTimeCardTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void InvalidTimeCardTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WorkingHoursTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void SickHoursTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void VacationHoursTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void OvertimeTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void ZeroOvertimeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTotalWorkingHoursTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void GetTotalSickHoursTest()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void GetTotalVacationHoursTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
